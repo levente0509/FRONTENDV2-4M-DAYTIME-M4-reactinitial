dcc507b9f28e27b2cedfc3900994c1bdbc42eacf
levente0509 <b.levente2@gmail.com>
2023-01-10 09:34:36 +0100

    loading works

--- reactinitial/src/App.js
+++ reactinitial/src/App.js
@@ -1,12 +1,21 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
+import LoadingMask from "./components/LoadingMask";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://demoapi.com/api/series/howimetyourmother")
+      .then((res) => res.json())
+      .then(() => setLoading(false));
+  }, []);
 
   return (
-    <div>
-      Todo...
+    <div className="App">
+      <h1>Series Api</h1>
+      {loading ? <LoadingMask /> : "loaded"}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
